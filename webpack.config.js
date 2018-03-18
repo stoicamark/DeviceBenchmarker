777ca2a12d8b90473a78e464c95244514bbb5d81
Stoica Mark <stoicamark@Stoica-MacBook-Pro.local>
2018-03-18 13:54:26 +0100

    refactor due to webpack API changes (4.1.1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 var path = require('path');
 
  module.exports = {
+   mode: "development",
    entry: './test/Main.test.ts',
    resolve: {
      extensions: ['.webpack.js', '.web.js', '.ts', '.js']
    },
    module: {
-     loaders: [
-       { test: /\.ts$/, loader: 'ts-loader' }
-     ]
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
    },
    output: {
      filename: 'bundle.js',
