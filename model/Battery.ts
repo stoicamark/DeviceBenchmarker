235c3be0c5cf74b437cbbc47bc2dea534178d67a
stoicamark <stoica.mark@yahoo.com>
2018-04-23 18:13:53 +0200

    refactor battery capacity handout

--- model/Battery.ts
+++ model/Battery.ts
@@ -2,12 +2,12 @@
  * Created by Habanero on 2017. 10. 06..
  */
 
-import {Event} from "../model/Event"
+import {Event} from "./Event";
 import { BenchmarkerClient } from "./ScoreComputer";
-declare let navigator : {getBattery: any}
+declare let navigator : {getBattery: any};
 
-declare function require(name:string) : any
-require('console.table')
+declare function require(name:string) : any;
+require('console.table');
 
 interface BatteryManager{
     level: number,
@@ -21,22 +21,19 @@ interface BatteryManager{
 
 export class Battery{
     
-    public OLDTChanged: Event<number> = new Event()
-    public ChargingChanged: Event<boolean> = new Event()
-    private _levelDropTimeInSeconds : number
-    private _avgLevelDropTimeInSeconds: number
-    private _isCharging : boolean
-    private _level : number
-    private _dischargingTimeInSeconds : number
-    private _initialTimeStamp : number
-    private _initialBatteryLevel : number
+    public OLDTChanged: Event<number> = new Event();
+    public ChargingChanged: Event<boolean> = new Event();
+    private _levelDropTimeInSeconds : number;
+    private _avgLevelDropTimeInSeconds: number;
+    private _isCharging : boolean;
+    private _level : number;
+    private _dischargingTimeInSeconds : number;
+    private _initialTimeStamp : number;
+    private _initialBatteryLevel : number;
 
-    private _client?: BenchmarkerClient
-    private _capacity?: number
-    private _actualCapacity?: number
-
-    private capacities: number[] = [4150, 4000, 3930, 3750, 3520,
-        3505, 3300, 3230, 3090, 3000, 2900, 2730, 2716, 2700, 2675, 1960, 1821]
+    private _client?: BenchmarkerClient;
+    private _capacity?: number;
+    private _actualCapacity?: number;
 
     // kiloByte per ses to Joules per s
     private bitrateToJoulesPerSec: {[keyBitrate : number]: number } = {
@@ -51,47 +48,47 @@ export class Battery{
         [200]:1.2,
         [225]:1.24,
         [250]:1.25,
-    }
+    };
 
-    private joulesToMiliamps(joules: number){
-        let volts = 5
-        return joules / (volts * 3.6) //mAh * volts * 3.6 = J
+    private static joulesToMiliamps(joules: number){
+        let volts = 5;
+        return joules / (volts * 3.6); //mAh * volts * 3.6 = J
     }
 
     private getMiliampsFromBitrate(bitrate: number) : number{
         //nearest multiple of 25kBps
-        let nearestMultiple = Math.floor(bitrate / 25) * 25
-        let joules = 0
+        let nearestMultiple = Math.floor(bitrate / 25) * 25;
+        let joules = 0;
         if(bitrate <= 250){
-            joules = this.bitrateToJoulesPerSec[nearestMultiple]
+            joules = this.bitrateToJoulesPerSec[nearestMultiple];
         }else{
             //from a given bitrate the consumed energy per MB is constant = 5 <J/MB>
-            joules = (bitrate * 5) / 1000
+            joules = (bitrate * 5) / 1000;
         }
-        return this.joulesToMiliamps(joules)
+        return Battery.joulesToMiliamps(joules);
     }
 
     constructor(client?: BenchmarkerClient){
-        this._avgLevelDropTimeInSeconds = 180 // one level drop in 3 minutes
-        this._initialTimeStamp = Date.now()
+        this._avgLevelDropTimeInSeconds = 180; // one level drop in 3 minutes
+        this._initialTimeStamp = Date.now();
 
         if(client === undefined){
-            this.subscribeForWebAPI()
+            this.subscribeForWebAPI();
         }else{
-            this.Client = client
+            this.Client = client;
         }
     }
 
     private subscribeForWebAPI(){
-        if(!this.isFunction(navigator.getBattery)){
-            console.log("The browser doesn't supports the battery interface (navigator.getBattery())")
-            return
+        if(!Battery.isFunction(navigator.getBattery)){
+            console.log("The browser doesn't supports the battery interface (navigator.getBattery())");
+            return;
         }
         navigator.getBattery().then((battery: BatteryManager) => {
-            this.initWithAllInfos(battery)
-            battery.onchargingchange = () => {this.updateChargeInfo(battery)}
-            battery.onlevelchange = () => {this.updateLevelInfo(battery)}
-            battery.ondischargingtimechange = () => {this.updateDischargingInfo(battery)}
+            this.initWithAllInfos(battery);
+            battery.onchargingchange = () => {this.updateChargeInfo(battery)};
+            battery.onlevelchange = () => {this.updateLevelInfo(battery)};
+            battery.ondischargingtimechange = () => {this.updateDischargingInfo(battery)};
         })
     }
 /*
@@ -111,150 +108,150 @@ export class Battery{
     }*/
 
     private initWithAllInfos(battery: BatteryManager){
-        this.updateChargeInfo(battery)
-        this.updateLevelInfo(battery)
-        this.updateDischargingInfo(battery)
+        this.updateChargeInfo(battery);
+        this.updateLevelInfo(battery);
+        this.updateDischargingInfo(battery);
     }
 
     private updateDischargingInfo(battery: BatteryManager){
-        this._dischargingTimeInSeconds = battery.dischargingTime
-        console.log("Battery discharging time: " + battery.dischargingTime + " seconds")
+        this._dischargingTimeInSeconds = battery.dischargingTime;
+        console.log("Battery discharging time: " + battery.dischargingTime + " seconds");
     }
 
     private updateLevelInfo(battery: BatteryManager){
-        this._level = battery.level
+        this._level = battery.level;
 
-        let actualTimeStamp = Date.now()
-        let dTime = actualTimeStamp - this._initialTimeStamp
-        this._initialTimeStamp = actualTimeStamp
+        let actualTimeStamp = Date.now();
+        let dTime = actualTimeStamp - this._initialTimeStamp;
+        this._initialTimeStamp = actualTimeStamp;
 
         if(this._levelDropTimeInSeconds === undefined){
-            this._levelDropTimeInSeconds = this._avgLevelDropTimeInSeconds
+            this._levelDropTimeInSeconds = this._avgLevelDropTimeInSeconds;
         }else{
-            this._levelDropTimeInSeconds = dTime / 1000 
-            this._levelDropTimeInSeconds = this._isCharging ? Infinity : this._levelDropTimeInSeconds
-            this.OLDTChanged.trigger(this._levelDropTimeInSeconds)
+            this._levelDropTimeInSeconds = dTime / 1000 ;
+            this._levelDropTimeInSeconds = this._isCharging ? Infinity : this._levelDropTimeInSeconds;
+            this.OLDTChanged.trigger(this._levelDropTimeInSeconds);
         } 
-        console.log("Battery level: " + battery.level * 100 + " %")
+        console.log("Battery level: " + battery.level * 100 + " %");
     }
 
     private updateChargeInfo(battery: BatteryManager){
-        this._isCharging = battery.charging
+        this._isCharging = battery.charging;
         if(this.level !== undefined){
-            this.ChargingChanged.trigger(this._isCharging)
+            this.ChargingChanged.trigger(this._isCharging);
         }
-        console.log("Battery charging? " + (battery.charging ? "Yes" : "No"))
+        console.log("Battery charging? " + (battery.charging ? "Yes" : "No"));
     } 
 
     get levelDropTimeInSeconds(): number {
-        return this._levelDropTimeInSeconds
+        return this._levelDropTimeInSeconds;
     }
 
     get isCharging(): boolean {
-        return this._isCharging
+        return this._isCharging;
     }
 
     get level(): number {
-        return this._level
+        return this._level;
     }
 
     get dischargingTimeInSeconds(): number {
-        return this._dischargingTimeInSeconds
+        return this._dischargingTimeInSeconds;
     }
 
     get Client(): BenchmarkerClient{
-        return this._client
+        return this._client;
     }
 
     set Client(value: BenchmarkerClient){
-        this._client = value
-        this.ChargingChanged.allOff()
-        this._capacity = this.capacities[Math.floor(Math.random() * this.capacities.length)]
-        this.reset()
+        this._client = value;
+        this.ChargingChanged.allOff();
+        this._capacity = this._client.getPeerFullCapacity();
+        this.reset();
     }
 
-    private numOfMaintenance: number
-    private sumOLDT: number
+    private numOfMaintenance: number;
+    private sumOLDT: number;
 
     public maintainBatteryInfos(dt: number){
         if(!this.Client){
             return
         }
-        this.numOfMaintenance++
+        this.numOfMaintenance++;
 
-        let bitrate = Math.floor(this.Client.getDownloadSpeed() + this.Client.getUploadSpeed())
+        let bitrate = Math.floor(this.Client.getDownloadSpeed() + this.Client.getUploadSpeed());
         
-        let energyConsumedInOneSec = this.getMiliampsFromBitrate(bitrate)
-        let energyConsumed = energyConsumedInOneSec * dt
+        let energyConsumedInOneSec = this.getMiliampsFromBitrate(bitrate);
+        let energyConsumed = energyConsumedInOneSec * dt;
         
-        this._actualCapacity -= Math.floor(energyConsumed)
-        this._level = Math.floor((this._actualCapacity / this._capacity) * 100) / 100
+        this._actualCapacity -= Math.floor(energyConsumed);
+        this._level = Math.floor((this._actualCapacity / this._capacity) * 100) / 100;
         
-        let drainedPercentage = energyConsumed / this._capacity
+        let drainedPercentage = energyConsumed / this._capacity;
         
-        let oldt = undefined
+        let oldt = undefined;
         
         if(drainedPercentage > 0){
-            oldt = Math.floor(dt / (drainedPercentage * 100))
-            this.sumOLDT += oldt
-            this.OLDTChanged.trigger(oldt)
-            let avgOLDT = this.sumOLDT / this.numOfMaintenance
-            this._dischargingTimeInSeconds = Math.floor(avgOLDT * this._level * 100)
+            oldt = Math.floor(dt / (drainedPercentage * 100));
+            this.sumOLDT += oldt;
+            this.OLDTChanged.trigger(oldt);
+            let avgOLDT = this.sumOLDT / this.numOfMaintenance;
+            this._dischargingTimeInSeconds = Math.floor(avgOLDT * this._level * 100);
         }
 
         let peerInfo = {
             property: "peerId",
             value: this.Client.getPeerId()
-        }
+        };
 
         let capacityInfo = {
             property: "capacity",
             value: this._actualCapacity + "/" + this._capacity + "(MAh)"
-        }
+        };
 
         let downloadInfo = {
             property: "download",
             value: this.Client.getDownloadSpeed() + "(KBps)"
-        }
+        };
 
         let uploadInfo = {
             property: "upload",
             value: this.Client.getUploadSpeed() + "(KBps)"
-        }
+        };
 
         let levelInfo = {
             property: "level",
             value: Math.floor(this._level * 100) + "(%)"
-        }
+        };
 
         let dischargingTimeInfo = {
             property: "discharging time",
             value: this._dischargingTimeInSeconds + "(s)"
-        }
+        };
 
         let OLDTInfo = {
             property: "OLDT",
             value: oldt + "(s/%)"
-        }
+        };
 
-        let tableInfo = [peerInfo, capacityInfo, downloadInfo, uploadInfo, levelInfo, dischargingTimeInfo, OLDTInfo]
-        console.table(tableInfo)
+        let tableInfo = [peerInfo, capacityInfo, downloadInfo, uploadInfo, levelInfo, dischargingTimeInfo, OLDTInfo];
+        console.table(tableInfo);
     }
 
     reset(){
-        this._actualCapacity = this._capacity
-        this._level = 1
-        this._isCharging = false
-        this.sumOLDT = 0
-        this.numOfMaintenance = 0
-        this._dischargingTimeInSeconds = 70000
+        this._actualCapacity = this._capacity;
+        this._level = 1;
+        this._isCharging = false;
+        this.sumOLDT = 0;
+        this.numOfMaintenance = 0;
+        this._dischargingTimeInSeconds = 70000;
     }
 
     get Capacity(): number{
-        return this._capacity
+        return this._capacity;
     }
 
-    private isFunction(functionToCheck: any) {
+    private static isFunction(functionToCheck: any) {
         const getType = {};
         return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
     }
--- model/ScoreComputer.ts
+++ model/ScoreComputer.ts
@@ -6,8 +6,8 @@ import {ConnectionType, Network} from "./Network"
 import {Event, Subscriber} from "./Event"
 import { Battery } from "./Battery";
 
-declare function require(name:string) : any
-require('console.table')
+declare function require(name:string) : any;
+require('console.table');
 
 export enum weights{
     batteryLevel = 0.3,
@@ -22,177 +22,178 @@ export interface BenchmarkerClient extends Subscriber<number>{
     getUploadSpeed():number
     getBandwidth():number
     getPeerId():String
+    getPeerFullCapacity():number
 }
 
 export class ScoreComputer implements Subscriber<boolean>{
     
-    private _network : Network
-    private _battery : Battery
-    private _actualScore : number
-    public _scoreAdjuster : ScoreAdjuster
+    private _network : Network;
+    private _battery : Battery;
+    private _actualScore : number;
+    public _scoreAdjuster : ScoreAdjuster;
 
-    _uplinkLimit : number
-    _downlinkLimit : number
-    _dischargingTimeLimit: number
+    _uplinkLimit : number;
+    _downlinkLimit : number;
+    _dischargingTimeLimit: number;
 
-    public ScoreComputed: Event<number> = new Event()
+    public ScoreComputed: Event<number> = new Event();
 
     constructor(battery?: Battery, network?: Network){
 
-        this._battery = battery === undefined ? new Battery() : battery
-        this._network = network === undefined ? new Network() : network
+        this._battery = battery === undefined ? new Battery() : battery;
+        this._network = network === undefined ? new Network() : network;
         
-        this._battery.ChargingChanged.on(this)
+        this._battery.ChargingChanged.on(this);
 
-        this._scoreAdjuster = new ScoreAdjuster(this._battery)
+        this._scoreAdjuster = new ScoreAdjuster(this._battery);
 
-        this._actualScore = 1
+        this._actualScore = 1;
 
-        this._downlinkLimit = 2.5 // maximum score coefficient if the downlink reaches >= 25 Mbps
-        this._uplinkLimit = 1 // maximum score coefficient if the uplink reaches >= 10 Mbps
-        this._dischargingTimeLimit = 7200 // maximum score coefficient if the discharging time >= two hour
+        this._downlinkLimit = 2.5; // maximum score coefficient if the downlink reaches >= 25 Mbps
+        this._uplinkLimit = 1; // maximum score coefficient if the uplink reaches >= 10 Mbps
+        this._dischargingTimeLimit = 7200; // maximum score coefficient if the discharging time >= two hour
     }
 
     public start(){
         setInterval(()=>{
             this.compute()
-        }, 10000)
+        }, 10000);
     }
 
     dataAvailable(batteryChargingInfo: boolean): void {
-        this.compute()
+        this.compute();
     }
 
     public subscribe( client: BenchmarkerClient) : void{
         if(this._network.Client === undefined){
-            this._network.Client = client
+            this._network.Client = client;
         }
         if(this._battery.Client === undefined){
-            this._battery.Client = client
+            this._battery.Client = client;
         }
-        this.ScoreComputed.on(client)
+        this.ScoreComputed.on(client);
     }
 
     public unsubscribe(client: BenchmarkerClient): void{
-        this._network.Client = undefined
-        this.ScoreComputed.off(client)
+        this._network.Client = undefined;
+        this.ScoreComputed.off(client);
     }
 
     public compute(): void{
 
-        let battery = this.Battery
-        let network = this.Network
+        let battery = this.Battery;
+        let network = this.Network;
                     
         if(network.type === ConnectionType.cellular || 
           (battery.level < 0.15 && !battery.isCharging)){
-            this._actualScore = 0
-            this.ScoreComputed.trigger(this._actualScore)
-            console.log("Network type: Cellular.")
-            console.log("OR")
-            console.log("Battery level is under 15% AND device is not charging.")
-            console.log("------(SCORE = 0)------")
-            return
+            this._actualScore = 0;
+            this.ScoreComputed.trigger(this._actualScore);
+            console.log("Network type: Cellular.");
+            console.log("OR");
+            console.log("Battery level is under 15% AND device is not charging.");
+            console.log("------(SCORE = 0)------");
+            return;
         }
         
         if((network.type === ConnectionType.wifi || 
             network.type === ConnectionType.ethernet) &&
             battery.isCharging){
 
-            this._actualScore = 1
-            this.ScoreComputed.trigger(this._actualScore)
+            this._actualScore = 1;
+            this.ScoreComputed.trigger(this._actualScore);
 
-            console.log("Network type: WIFI OR ETHERNET.")
-            console.log("AND")
-            console.log("Device is charging.")
-            console.log("------(SCORE = 1)------")
-            return
+            console.log("Network type: WIFI OR ETHERNET.");
+            console.log("AND");
+            console.log("Device is charging.");
+            console.log("------(SCORE = 1)------");
+            return;
         }
                     
-        let score = 0
-        let infoTable = []
+        let score = 0;
+        let infoTable = [];
         
-        score += battery.level * weights.batteryLevel
+        score += battery.level * weights.batteryLevel;
         
         let levelInfo = { 
             property: "battery level",
             value: battery.level,
             weigth: weights.batteryLevel,
             scoreTag: battery.level * weights.batteryLevel
-        }
+        };
 
-        infoTable.push(levelInfo)
+        infoTable.push(levelInfo);
         
-        let dischargingTimeScore = battery.dischargingTimeInSeconds / this._dischargingTimeLimit
-        dischargingTimeScore = dischargingTimeScore < 1 ? dischargingTimeScore : 1
-        score += dischargingTimeScore * weights.batteryDischargingTime
+        let dischargingTimeScore = battery.dischargingTimeInSeconds / this._dischargingTimeLimit;
+        dischargingTimeScore = dischargingTimeScore < 1 ? dischargingTimeScore : 1;
+        score += dischargingTimeScore * weights.batteryDischargingTime;
             
-        let dischargeInfo = { 
+        let dischargeInfo = {
             property: "discharging time score",
             value: dischargingTimeScore,
             weigth: weights.batteryDischargingTime,
             scoreTag: dischargingTimeScore * weights.batteryDischargingTime
-        }
-        infoTable.push(dischargeInfo)
+        };
+        infoTable.push(dischargeInfo);
 
-        let dSpeedScore = (network.downlink / this._downlinkLimit)
-        let uSpeedScore = (network.uplink / this._uplinkLimit)
+        let dSpeedScore = (network.downlink / this._downlinkLimit);
+        let uSpeedScore = (network.uplink / this._uplinkLimit);
         
-        dSpeedScore = dSpeedScore < 1 ? dSpeedScore : 1
-        uSpeedScore = uSpeedScore < 1 ? uSpeedScore : 1
+        dSpeedScore = dSpeedScore < 1 ? dSpeedScore : 1;
+        uSpeedScore = uSpeedScore < 1 ? uSpeedScore : 1;
         
-        score += dSpeedScore * weights.downloadSpeed
-        score += uSpeedScore * weights.uploadSpeed
-        score += this._scoreAdjuster.adjustment * weights.adjustment
+        score += dSpeedScore * weights.downloadSpeed;
+        score += uSpeedScore * weights.uploadSpeed;
+        score += this._scoreAdjuster.adjustment * weights.adjustment;
 
-        score = score < 0 ? 0 : score
+        score = score < 0 ? 0 : score;
         
-        this._actualScore = score
-        this.ScoreComputed.trigger(score)
+        this._actualScore = score;
+        this.ScoreComputed.trigger(score);
 
         let downlinkInfo = { 
             property: "downlink",
             value: dSpeedScore,
             weigth: weights.downloadSpeed,
             scoreTag: dSpeedScore * weights.downloadSpeed
-        }
+        };
 
         let uplinkInfo = { 
             property: "uplink",
             value: uSpeedScore,
             weigth: weights.uploadSpeed,
             scoreTag: uSpeedScore * weights.uploadSpeed
-        }
+        };
             
         let adjusterInfo = { 
             property: "adjustment",
             value: this._scoreAdjuster.adjustment,
             weigth: weights.adjustment,
             scoreTag: this._scoreAdjuster.adjustment * weights.adjustment
-        }
+        };
 
         let scoreInfo = {
             property: "TOTAL SCORE",
             value: "----",
             weigth: "----",
             scoreTag: score
-        }
+        };
 
-        infoTable.push(downlinkInfo, uplinkInfo, adjusterInfo, scoreInfo)
-        console.table(infoTable)
+        infoTable.push(downlinkInfo, uplinkInfo, adjusterInfo, scoreInfo);
+        console.table(infoTable);
     }
 
     public get ActualScore() : number{
-        return this._actualScore
+        return this._actualScore;
     }
 
     public get Battery() : Battery{
         if(this.ScoreComputed.hasListener){
-            this._battery.maintainBatteryInfos(10)
+            this._battery.maintainBatteryInfos(10);
         }
-        return this._battery
+        return this._battery;
     }
 
     public get Network(): Network{
-        return this._network
+        return this._network;
     }
 }
